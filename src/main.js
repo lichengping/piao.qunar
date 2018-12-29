d396c148fe7e64b396aee8d33866c5d4730bf1c3
join cheng <13265463912@163.com>
2018-12-29 19:35:17 +0800

    第二提交去哪儿网

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import App from './App'
 import router from './router'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
---
+++ src/pages/home/components/header.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="header">
+  <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+  </div>
+  <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市景点游玩主题
+</div>
+  <div class="header-right">
+      城市
+      <span class="iconfont">&#xe64a;</span>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+<style scoped>
+  .header{
+    display: flex;
+    background: #00bcd4;
+    line-height: 0.86rem;
+    /* text-align: center; */
+    color: #fff;
+  }
+  .header .header-left{
+    width: 0.64rem;
+  }
+  .header-left .back-icon{
+      text-align: center;
+      font-size: 0.4rem;
+  }
+  .header .header-right{
+    width: 1.24rem;
+  }
+  .header .header-input{
+    flex: 1;
+    background: #fff;
+    border-radius: .1rem;
+    height: 0.64rem;
+    line-height: 0.64rem;
+    margin-top:.11rem;
+    margin-left:0.1rem; 
+    padding-left: 0.1rem;
+    color: #ccc; 
+  }
+</style>
