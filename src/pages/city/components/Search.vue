80bb0747910b8570bee74ebe520cb3d3c1c3df44
join cheng <13265463912@163.com>
2019-01-06 21:49:34 +0800

    索搜

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或拼音">
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'CitySearch'
+}
+</script>
+
+<style scoped>
+   .search {
+     height: .72rem;
+     padding: 0 .1rem;
+     background: #00bcd4;
+   }
+   .search-input {
+     box-sizing: border-box;
+     height: .62rem;
+     width: 100%;
+     line-height: .62rem;
+     border-radius: 0.08rem;
+     padding: 0 .1rem; 
+     color: #666;
+   }
+</style>
